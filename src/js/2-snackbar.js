9b60d0a4ed1e80b5c99ce352036ada691238d09d
Vladyslav Yevdokymenko <evdokimenko@hotmail.com>
2026-08-12 12:31:57 +0300

    fixed message creating

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,5 +1,5 @@
 import iziToast from 'izitoast';
-import { getErrorSettings, getSuccessSettings } from './snackbar-helper.js';
+import spriteUrl from '../img/sprite.svg';
 
 import '../css/2-snackbar.css';
 import 'izitoast/dist/css/iziToast.min.css';
@@ -27,17 +27,53 @@ form.addEventListener('submit', (e) => {
     delay: delay,
     shouldResolve: state === 'fulfilled',
   })
-    .then(delay =>
+    .then(delay => {
       iziToast.show({
-        ...getSuccessSettings(delay),
+        title: 'OK',
+        titleColor: '#FFFFFF',
         message: `Fulfilled promise in ${delay}ms`,
-      })
-    )
-    .catch(delay =>
+        messageColor: '#FFFFFF',
+        position: 'topRight',
+        transitionIn: 'fadeIn',
+        animateInside: false,
+        backgroundColor: '#59a10d',
+        color: '#fff',
+        icon: 'toast-icon',
+        class: 'snackbar-toast',
+        progressBarColor: '#326101',
+        onOpening(instance, toast) {
+          const icon = toast.querySelector('.iziToast-icon');
+          icon.innerHTML = `
+            <svg width="24" height="24" aria-hidden="true">
+              <use href="${spriteUrl}#x-success"></use>
+            </svg>
+          `;
+        },
+      });
+    })
+    .catch(delay => {
       iziToast.show({
-        ...getErrorSettings(delay),
+        title: 'Error',
+        titleColor: '#FFFFFF',
         message: `Rejected promise in ${delay}ms`,
-      })
-    );
+        messageColor: '#FFFFFF',
+        position: 'topRight',
+        transitionIn: 'fadeIn',
+        animateInside: false,
+        backgroundColor: '#ef4040',
+        color: '#fff',
+        icon: 'toast-icon',
+        class: 'snackbar-toast',
+        progressBarColor: '#b51b1b',
+        onOpening(instance, toast) {
+          const icon = toast.querySelector('.iziToast-icon');
+          icon.innerHTML = `
+            <svg width="24" height="24" aria-hidden="true">
+              <use href="${spriteUrl}#x-octagon"></use>
+            </svg>
+          `;
+        },
+      });
+    });
   form.reset();
 });
