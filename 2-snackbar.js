4721056566ee4b327f0b8b3fe4f8c4236ad7733d
vyevdokymenko <evdokimenko@hotmail.com>
2026-08-12 09:26:34 +0000

    Deploying to gh-pages from @ vyevdokymenko/goit-js-hw-10@80e839066b300f9e9abb54d56386869f226d1b91 🚀

--- 2-snackbar.js
+++ 2-snackbar.js
@@ -1,10 +1,10 @@
-import{s as a}from"./assets/sprite-Dq4561ti.js";/* empty css                      */import{i as n}from"./assets/vendor-BbbuE1sJ.js";const c={titleColor:"#FFFFFF",messageColor:"#FFFFFF",position:"topRight",transitionIn:"fadeIn",animateInside:!1,color:"#fff",icon:"toast-icon",class:"snackbar-toast"},l=t=>{const s={title:"OK",message:`Fulfilled promise in ${t}ms`,backgroundColor:"#59a10d",progressBarColor:"#326101",onOpening(o,e){const r=e.querySelector(".iziToast-icon");r.innerHTML=`
+import{s as a}from"./assets/sprite-Dq4561ti.js";/* empty css                      */import{i as n}from"./assets/vendor-BbbuE1sJ.js";const c={titleColor:"#FFFFFF",messageColor:"#FFFFFF",position:"topRight",transitionIn:"fadeIn",animateInside:!1,color:"#fff",icon:"toast-icon",class:"snackbar-toast"},l=s=>{const t={title:"OK",message:`Fulfilled promise in ${s}ms`,backgroundColor:"#59a10d",progressBarColor:"#326101",onOpening(o,e){const i=e.querySelector(".iziToast-icon");i.innerHTML=`
        <svg width="24" height="24" aria-hidden="true">
         <use href="${a}#x-success"></use>
        </svg>
-    `}};return{...c,...s}},u=t=>{const s={title:"Error",message:`Rejected promise in ${t}ms`,backgroundColor:"#ef4040",progressBarColor:"#b51b1b",onOpening(o,e){const r=e.querySelector(".iziToast-icon");r.innerHTML=`
+    `}};return{...c,...t}},m=s=>{const t={title:"Error",message:`Rejected promise in ${s}ms`,backgroundColor:"#ef4040",progressBarColor:"#b51b1b",onOpening(o,e){const i=e.querySelector(".iziToast-icon");i.innerHTML=`
        <svg width="24" height="24" aria-hidden="true">
         <use href="${a}#x-octagon"></use>
        </svg>
-    `}};return{...c,...s}},i=document.querySelector(".form"),m=({value:t,delay:s,shouldResolve:o=!0})=>new Promise((e,r)=>{setTimeout(()=>{o?e(t):r(t)},s)});i.addEventListener("submit",t=>{t.preventDefault();const s=Math.max(Number(i.elements.delay.value),0),o=i.elements.state.value;m({value:s,delay:s,shouldResolve:o==="fulfilled"}).then(e=>n.show(l(e))).catch(e=>n.show(u(e))),i.reset()});
+    `}};return{...c,...t}},r=document.querySelector(".form"),u=({value:s,delay:t,shouldResolve:o=!0})=>new Promise((e,i)=>{setTimeout(()=>{o?e(s):i(s)},t)});r.addEventListener("submit",s=>{s.preventDefault();const t=Math.max(Number(r.elements.delay.value),0),o=r.elements.state.value;u({value:t,delay:t,shouldResolve:o==="fulfilled"}).then(e=>n.show({...l(e),message:`Fulfilled promise in ${e}ms`})).catch(e=>n.show({...m(e),message:`Rejected promise in ${e}ms`})),r.reset()});
 //# sourceMappingURL=2-snackbar.js.map
